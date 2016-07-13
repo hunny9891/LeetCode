8f7c728086ae761bc82e31795e58c36019a0014c
hunny9891 <himanshu_thakur9891@live.in>
2016-07-13 19:28:19 +0530

    Updated pascal triangle

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -5,27 +5,37 @@ import java.util.List;
 
 public class PascalTriangle {
 	public List<List<Integer>> generate(int numRows) {
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		if(numRows == 1) {
-			ArrayList<Integer> innerList = new ArrayList<>();
-			innerList.add(1);
-			result.add(innerList);
+		
+		ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+		ArrayList<Integer> innerList = null;
+		for(int i=1;i<=numRows;i++) {
+			if(i == 1) {
+				innerList = new ArrayList<Integer>();
+				innerList.add(1);
+				result.add(innerList);
+			}
+			else if(i == 2) {
+				innerList = new ArrayList<Integer>();
+				innerList.add(1);
+				innerList.add(1);
+				result.add(innerList);
+			}
+			else {
+				innerList = new ArrayList<Integer>();
+				innerList.add(1);
+				List<Integer> previousList = result.get(i-2);
+				for(int j=1;j<previousList.size();j++) {
+					innerList.add(previousList.get(j - 1) + previousList.get(j));
+				}
+				innerList.add(1);
+				result.add(innerList);
+			}
 		}
-		else if(numRows == 2) {
-			ArrayList<Integer> innerList = new ArrayList<>();
-			innerList.add(1);
-			innerList.add(1);
-			result.add(innerList);
-			
-		}
-		else {
-		}
-		return null;
-	}
-
+        return result;
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PascalTriangle t = new PascalTriangle();
+		System.out.println(t.generate(9).toString());
 	}
 
 }
